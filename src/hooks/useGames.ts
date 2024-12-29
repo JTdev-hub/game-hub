580fe14fd0716cc2f5c6399603d8b54f9acb0798
Ranel <jrrtuble@gmail.com>
2024-12-29 08:34:19 +0800

    Show Loading Skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -5,22 +5,28 @@ import gameService, {
 } from "../services/game-service";
 import { CanceledError } from "axios";
 const useGames = () => {
+  const [isLoading, setLoading] = useState(false);
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
 
   useEffect(() => {
+    setLoading(true);
     const { request, cancel } = gameService.getAll<FetchGamesResponse>();
     request
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
 
     return () => cancel();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
