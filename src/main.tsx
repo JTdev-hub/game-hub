834b9038d7938ed77b9633a21264d69d9cc18654
Ranel <jrrtuble@gmail.com>
2025-04-22 12:38:30 +0800

    latest commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import App from "./App.tsx";
 import theme from "./theme.ts";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <App />
+      </ChakraProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
